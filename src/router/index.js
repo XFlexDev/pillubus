40d1cc4792fea97ea6da783a3bec750a80fe4152
XFlexDev <145843742+XFlexDev@users.noreply.github.com>
2026-01-14 18:54:00 +0200

    Update ticket routes in the Vue router

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
+
 import TicketsBuy from "../views/TicketsBuy.vue";
 import SingleTicket from "../views/SingleTicket.vue";
 import PaymentProcessing from "../views/PaymentProcessing.vue";
 import YourTickets from "../views/YourTickets.vue";
+import Ticket from "../views/Ticket.vue";
+import Settings from "../views/Settings.vue";
 
 export default createRouter({
   history: createWebHistory(),
@@ -10,6 +13,8 @@ export default createRouter({
     { path: "/", component: TicketsBuy },
     { path: "/single", component: SingleTicket },
     { path: "/processing", component: PaymentProcessing },
-    { path: "/tickets/yours", component: YourTickets },
+    { path: "/tickets", component: YourTickets },
+    { path: "/ticket/:id", component: Ticket },
+    { path: "/settings", component: Settings },
   ],
 });
