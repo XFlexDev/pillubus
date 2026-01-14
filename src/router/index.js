c6713fc03d8640e232d8ec1395c6535e1d92c113
XFlexDev <145843742+XFlexDev@users.noreply.github.com>
2026-01-14 18:07:48 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
-
 import TicketsBuy from "../views/TicketsBuy.vue";
-import SingleTicket from "../views/SingleTicket.vue";
-import YourTickets from "../views/YourTickets.vue";
-import Routes from "../views/Routes.vue";
-import More from "../views/More.vue";
-import Settings from "../views/Settings.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: "/", redirect: "/tickets" },
-
-    { path: "/tickets", component: TicketsBuy },
-    { path: "/tickets/single", component: SingleTicket },
-    { path: "/tickets/yours", component: YourTickets },
-
-    { path: "/routes", component: Routes },
-    { path: "/more", component: More },
-    { path: "/settings", component: Settings },
+    {
+      path: "/",
+      component: TicketsBuy,
+    },
   ],
 });
 
