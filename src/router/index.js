da80994adeb448a4c64b0bd185a93804d94963ea
XFlexDev <145843742+XFlexDev@users.noreply.github.com>
2026-01-14 18:29:34 +0200

    Add PaymentProcessing route to the router

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 import TicketsBuy from "../views/TicketsBuy.vue";
 import SingleTicket from "../views/SingleTicket.vue";
+import PaymentProcessing from "../views/PaymentProcessing.vue";
 
 export default createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", component: TicketsBuy },
     { path: "/single", component: SingleTicket },
+    { path: "/processing", component: PaymentProcessing },
   ],
 });
