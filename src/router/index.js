e531f60cf257c178a816f2d0c5cc349c03e9b73f
XFlexDev <145843742+XFlexDev@users.noreply.github.com>
2026-01-14 18:38:41 +0200

    Add route for YourTickets view

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import TicketsBuy from "../views/TicketsBuy.vue";
 import SingleTicket from "../views/SingleTicket.vue";
 import PaymentProcessing from "../views/PaymentProcessing.vue";
+import YourTickets from "../views/YourTickets.vue";
 
 export default createRouter({
   history: createWebHistory(),
@@ -9,5 +10,6 @@ export default createRouter({
     { path: "/", component: TicketsBuy },
     { path: "/single", component: SingleTicket },
     { path: "/processing", component: PaymentProcessing },
+    { path: "/tickets/yours", component: YourTickets },
   ],
 });
