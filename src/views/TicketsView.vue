82b868809f358f05724ceefb1c75080ff2b54c30
XFlexDev <145843742+XFlexDev@users.noreply.github.com>
2026-01-16 16:17:22 +0200

    Add TicketsView component with ticket purchase prompt

---
+++ src/views/TicketsView.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container class="pt-6 pb-16">
+    <h1 class="mb-4">Tickets</h1>
+    <v-alert type="info" variant="tonal">
+      No tickets yet. Buy your first one.
+    </v-alert>
+
+    <v-btn class="mt-4" color="primary" @click="$router.push('/buy')" ripple>
+      Buy ticket
+    </v-btn>
+  </v-container>
+</template>
