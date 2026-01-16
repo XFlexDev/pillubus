331b6d2c97e3dd44392c4e20095825a7b213e45f
XFlexDev <145843742+XFlexDev@users.noreply.github.com>
2026-01-16 16:18:01 +0200

    Add SettingsView component with basic settings UI

---
+++ src/views/SettingsView.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container class="pt-6 pb-16">
+    <h1 class="mb-4">Settings</h1>
+
+    <v-text-field label="Your name" />
+
+    <v-switch
+      label="Dark mode"
+      class="mt-2"
+    />
+  </v-container>
+</template>
